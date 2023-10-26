d4f90d46714d8ab850cac8f5189d1e61699deff3
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2023-10-26 17:45:52 +0700

    merge

--- main.js
+++ main.js
@@ -3,12 +3,24 @@ const htmlDminimalism = elementDminimalism.innerHTML
 
 const elementSalingSilang = document.querySelector('#SalingSilang')
 const htmlSalingSilang = elementSalingSilang.innerHTML
+
+const elementAngkutBrangkas = document.querySelector('#AngkutBrangkas')
+const htmlAngkutBrangkas = elementAngkutBrangkas.innerHTML
+
 console.log('baris 6')
 elementSalingSilang.innerHTML = ''
+elementAngkutBrangkas.innerHTML=''
+
 
 function emptyContent() {
     elementDminimalism.innerHTML = ''
     elementSalingSilang.innerHTML = ''
+    elementAngkutBrangkas.innerHTML=''
+}
+
+function showAngkutBrangkas() {
+    emptyContent()
+    elementAngkutBrangkas.innerHTML = htmlAngkutBrangkas
 }
 
 function showSalingSilang() {
